52b4aed431d89e21a941f82168a769a2eaa89831
lucaslingle <lucasdaxlingle@gmail.com>
2021-05-20 20:35:35 -0700

    dev-1 : commit updates

--- main.py
+++ main.py
@@ -25,5 +25,3 @@ def main(args):
 if __name__ == '__main__':
     args = parse_args()
     main(args)
-
-
--- utils/comm_util.py
+++ utils/comm_util.py
@@ -1,5 +1,6 @@
 from mpi4py import MPI
 import torch as tc
+import numpy as np
 
 
 def get_comm():
@@ -12,4 +13,9 @@ def sync_params(agent, comm):
         comm.Bcast(p_data, root=0)
         p.copy_(tc.FloatTensor(p_data))
 
-def mpi_mean()
+def sync_grads(agent, comm):
+    for p in agent.parameters():
+        p_grad_local = p.grad.numpy()
+        p_grad_global = np.zeros_like(p_grad_local)
+        comm.Allreduce(sendbuf=p_grad_local, recvbuf=p_grad_global, op=MPI.SUM)
+        p.grad.copy_(tc.FloatTensor(p_grad_global) / comm.Get_size())
