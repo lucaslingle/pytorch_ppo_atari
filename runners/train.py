250448c5ce711307b9ac97f4d183483409dfd108
lucaslingle <lucasdaxlingle@gmail.com>
2021-05-26 04:26:33 -0700

    fix a docstring

--- runners/train.py
+++ runners/train.py
@@ -248,7 +248,7 @@ def _train(env, agent, args):
 
     :param env: openai gym environment or wrapper thereof.
     :param agent: agent_util.Agent encapsulating a trainable model, an optimizer, a scheduler,
-        and the mpi4py WORLD_COMM communicator.
+        and the mpi4py COMM_WORLD communicator object.
     :param args: argparsed args.
     :return:
     """
